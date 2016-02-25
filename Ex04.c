94d37cb20d4088b55e88431fbaaee1e1d854b888
Anders Voldsund <andersvoldsund@gmail.com>
2016-02-25 20:13:07 +0100

    Removed some comments. Included check for number of processes a power of 2.

--- Ex04.c
+++ Ex04.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include "mpi.h"
 #include <omp.h>
+#include <stdbool.h>
  
 void compute_v(int n, double *v) {
 	int i;
@@ -30,17 +31,32 @@ double compute_error(double S_n) {
 	return fabs(S - S_n);
 }
 
+bool isPowerOfTwo(unsigned int x) {
+
+    while( ((x % 2) == 0) && x > 1 ) {
+        x /= 2;
+    }
+
+    return (x == 1);
+}
+
 
 int main(int argc, char ** argv) {
 
-
 	MPI_Init(&argc, &argv);
 	int P, rank;
-	MPI_Comm_size(MPI_COMM_WORLD, &P); // size = number of processes nprocs
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Numbering of process
+	MPI_Comm_size(MPI_COMM_WORLD, &P); // P = number of processes
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Rank = numbering of each process
+
+    if (isPowerOfTwo(P) == false) {
+        if (rank == 0) {
+            printf("The number of processes is not a power of two \n");
+        }        
+        MPI_Finalize();
+        return 1;
+    }
 
 	if (argc < 2) {
-		// Only one process needs to print usage output
 		if (rank == 0) {
 			printf("Usage: ex3 k, n=pow(2,k)\n");
 			printf("n = vector/matrix size\n");
@@ -54,7 +70,6 @@ int main(int argc, char ** argv) {
 	int n = (int) pow(2, k);
 	
 	int np = (int) (n - n%P)/P;
-	//printf("n mod P = %d\n", n%P);
 	
 	double *v, S = 0.0;
 
@@ -66,7 +81,7 @@ int main(int argc, char ** argv) {
 		compute_v(n, v);
 			
 		for (i = 1; i < P; i++ ){
-			MPI_Send( &v[np*i], np, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+			MPI_Send(&v[np*i], np, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
 		}
 		double S_n = compute_S(np, v);
 		MPI_Reduce(&S_n, &S, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
@@ -76,7 +91,6 @@ int main(int argc, char ** argv) {
 		printf("Error = %1.16f\n", err);
 
 		free(v);
-		//int n_check = (P)*np + n%P;
 
 	}
 	else {
@@ -84,28 +98,19 @@ int main(int argc, char ** argv) {
 		double *v_n;
 		if (rank == P-1) {
 			v_n = calloc(np + n%P, sizeof(double));
-			//printf("np + mod = %d\n", np + n%P);
 		}
 		else {
 			v_n = calloc(np, sizeof(double));
-			//printf("np = %d\n", np);
 		}
 		MPI_Recv(v_n, np, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
 
-
-
 		double S_n = compute_S(np, v_n);
-
 		free(v_n);
 
 		MPI_Reduce(&S_n, &S, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 	}
 
-	
 	MPI_Finalize();
 
-
 	return 0;
-
-
 }
